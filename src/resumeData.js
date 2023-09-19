6899913cbe66852314a20742c2e6f91765876a1a
r0bxz <m.alrob1@student.aaup.edu>
2023-09-19 03:54:24 +0300

    portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,79 @@
+let resumeData = {
+    "name": "MAHMOUD ABU ALROB",
+     "Phone":"+970593876221",
+    "role": "Machine Learning Engineer",
+    "Email":"m.mahmoud.rob@gmail.com",
+    "roleDescription": " I like designing and developing machine learning systems",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/mahmoud-abu-alrob-146644215/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/r0bxz",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://twitter.com/r0b69226766",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"5th-year Computer System Engineering student at Arab American University with a good knowledge of programming languages and development tools, seeking an internship that matches my experience and present new challenges. ",
+    "address":"Jenin-Palestine",
+    "education":[
+      {
+        "UniversityName":"Arab American University",
+        "specialization":"Computer System Enigneering",
+        "MonthOfPassing":"Oct",
+        "YearOfPassing":"2023",
+        "Achievements":"3.2 GPA"
+      },
+      {
+        "UniversityName":"English Access  Microscholarship",
+        "specialization":"Advanced English",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2015",
+        "Achievements":"TOEFL Junior"
+      }
+    ],
+    "skills":[
+      {
+        "skillname":"C++"
+      },
+      {
+        "skillname":"PYTHON"
+      },
+      {
+        "skillname":"JAVA"
+      }
+    
+    ],
+    "portfolio":[
+      {
+        "name":"Analysis of Adult Income Dataset Using Support Vector Machines and Neural Networks",
+        "description":"ML project",
+        "imgurl":"images/portfolio/ML.JPG"
+      },
+      {
+        "name":"Smart Parking Application",
+        "description":"Software Project",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"Smart Driving Monitoring System",
+        "description":"Senior Project",  
+        "imgurl":"images/portfolio/smart.jpg"
+      },
+      {
+        "name":"Beita",
+        "description":"Flutter Application",  
+        "imgurl":"images/portfolio/icon.jpg"
+      }
+    ],
+    
+  }
+  
+  export default resumeData
